c1656d8805f1d86921817fa48cac266aeecfda45
Dominic Aaron <keepinitclose@gmail.com>
2026-04-29 16:26:55 -0500

    feat(18-custom-actions): add action.yaml and python

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,2 +1,39 @@
-if __name__ == "__main__":
+import os
+from urllib import request
+import requests
+import time
+
+def ping_url(url, delay, max_trials):
+    trials = 0
+
+    while trials < max_trials:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                print(f"Website {url} is reachable.")
+                return True
+        except requests.ConnectionError:
+            print(f"Website {url} is unreachable. Retrying in {delay} seconds...")
+            time.sleep(delay)
+            trials += 1
+        except requests.exceptions.MissingSchema:
+            print(f"Invalid URL format {url}. Make sure the URL has a valid scheme (http:// or https://).")
+            return False
+
+    return False
+
+def run():
+    website_url = os.getenv("INPUT_URL")
+    delay = int(os.getenv("INPUT_DELAY"))
+    max_trials = int(os.getenv("INPUT_MAX_TRIALS"))
+
+    website_reachable = ping_url(website_url, delay, max_trials)
     print("Hello, World!")
+
+    if not website_reachable:
+        raise Exception(f"Website {website_url} is malformed or unreachable.")
+    
+    print(f"Website {website_url} is reachable after.")
+
+if __name__ == "__main__":
+    run()
